03374de0899e46839efd2e2e4d0cd654ce048dde
ratulahmed <ratulahmed1@gmail.com>
2021-09-11 22:49:26 -0400

    created logic for user selections + some styling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DashboardService } from './dashboard.service';
 
 import { Odds } from './odds.model';
+import { UserSelection } from './user-selection.model';
 
-const ELEMENT_DATA: Odds[] = [
-  // {week: 1, homeTeam: 'CAR Panthers',homeSpread:'-5.5', awayTeam: 'NY Jets', awaySpread: '+5.5'}
-  // {week: 1, homeTeam: 'BUF Bills',homeSpread:'-6.5', awayTeam: 'PIT Steelers', awaySpread: '+6.5'},
-  // {week: 1, homeTeam: 'DET Lions',homeSpread:'+7', awayTeam: 'SF 49ers', awaySpread: '-7'},
-  // {week: 1, homeTeam: 'HOU Texans', homeSpread:'+3',awayTeam: 'JAX Jaguars', awaySpread: '-3'},
-  // {week: 1, homeTeam: 'IND Colts', homeSpread:'+2.5',awayTeam: 'SEA Seahawks', awaySpread: '+2.5'},
-];
+const ELEMENT_DATA: Odds[] = [];
 
 @Component({
   selector: 'app-dashboard',
@@ -20,9 +16,9 @@ export class DashboardComponent implements OnInit {
 
   displayedColumns: string[] = ['home_team', 'home_spread', 'away_team', 'away_spread', 'selections'];
   dataSource = ELEMENT_DATA;
-  selections : any[] = [];
+  selections : UserSelection[] = [];
 
-  constructor(public dashboardService : DashboardService) { }
+  constructor(public dashboardService : DashboardService, public router : Router) { }
 
   ngOnInit(): void {
     this.getOdds();
@@ -39,8 +35,6 @@ export class DashboardComponent implements OnInit {
     getSelections() {
       this.dashboardService.fetchSelections()
         .subscribe(res => {
-          console.log(res);
-
           for(let i = 0; i < this.dataSource.length; i++) {
             for(let j = 0; j < res.length; j++){
               if(this.dataSource[i].id === res[j].odd_id) {
@@ -48,16 +42,30 @@ export class DashboardComponent implements OnInit {
               }
             }
           }
-          console.log(this.dataSource);
-
         })
     }
 
     onSubmit() {
+      this.dashboardService.submitChoices(this.selections)
+        .subscribe(res => {
+          console.log(res);
+        })
+      this.selections = [];
     }
 
-    onSelect(value: any) {
+    onSelect(value: string, oddId: number) {
+      // create a request json of objects that have
+      // userId: oddid, selection
+      let newSelection = <UserSelection>{};
+      newSelection.user_id = localStorage.getItem('userId');
+      newSelection.odd_id = oddId;
+      newSelection.selection = value;
 
-      console.log(value);
+      if(value === 'No Selection') {
+        newSelection.selection = null;
+      }
+      // TODO this will push literally all selections - maybe update
+      //  the ones that currently exists - still works e2e however
+      this.selections.push(newSelection);
     }
   }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -2,22 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Odds } from './odds.model';
+import { UserSelection } from './user-selection.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-
   private fetchedOdds : Odds[] = []
+
   constructor(private http: HttpClient) { }
 
+
+  submitChoices(selections: UserSelection[]) : Observable<any> {
+    console.log(selections);
+    return this.http.post('http://localhost:3000/api/userChoice', selections);
+  }
 
   fetchOdds() : Observable<Odds[]> {
     return this.http.get<Odds[]>('http://localhost:3000/api/odd');
   }
 
   fetchSelections() : Observable<any> {
-    return this.http.get('http://localhost:3000/api/odd/test')
+    return this.http.get('http://localhost:3000/api/odd/' +
+    localStorage.getItem('userId'));
   }
 }
